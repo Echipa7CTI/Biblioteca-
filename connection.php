5c62b6e9a56d4edde70e333cc74a9a98713c1abe
mileacostin <59766322+mileacostin@users.noreply.github.com>
2020-01-11 16:04:26 +0200

    Delete connection.php

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
- $db = mysqli_connect('localhost','admin','Parola123','biblioteca')
- or die('Error connecting to MySQL server.');
- echo '<script>console.log("Connected to database!")</script>';
-
-
-?>
